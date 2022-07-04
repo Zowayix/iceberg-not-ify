c178e30f9c70f43a1c8fce36757dfe74a0414a0e
Megan Leet <megan.leet@iinet.net.au>
2022-07-04 20:31:29 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# icebergify
-obscurity iceberg generation tool using spotify web api
+# iceberg-not-ify
+
+Fork of the original icebergify for people who are too cool to use Spotify. This isn't hosted anywhere because I can't really be arsed to put it on some kind of free hosting service, but it's here for people who want to run Flask locally.
+
+Still ends up using the Spotify API to get popularity, as it seems to be the best way to do that. Was looking at using Discogs's API (and determining popularity by the total number of users having/wanting a release for all releases by an artist), but without an artist ID you would have to use the search API which is the one thing you can't use without authentication. MusicBrainz probably would have worked, but the number of user rating votes for an artist isn't really enough to get meaningful results out of. So ironically enough, I still had to create a Spotify account and now I don't get to tell people I don't have one…
+
+It only needs an application registered (to use the client ID and secret) though, which can be provided by the SPOTIFY_CLIENT_ID and SPOTIFY_SECRET environment variables, so at least all the code relating to user authentication can be taken out. This isn't a very elegant hack/fork with much effort put into it, just rips out any no longer relevant code slaps a form on the opening page for entering name (normally from the Spotify account) and to upload a JSON file containing historical play data; also instead of needing the data/ directory it writes the iceberg as a data: URL instead, and there is now a hidden layer below the iceberg (no graphics, just red text on a black background) to show any artists that are not found on Spotify and hence have no popularity (for ultimate hipster points). The stuff in privacy.html is probably no longer accurate or relevant now that there's no Spotify user account to be accessed, but I didn't touch that because I'm not a lawyer and might not know what I'm doing.
+
+JSON file format: Object/dictionary like this:
+
+```json
+{
+	"Album Artist/Album/1-01 Title.ogg": {
+		"artist": "Artist",
+		"album": "Album",
+		"title": "Title",
+		"genre": "Genre",
+		"year": 1990,
+		"plays": [
+			"2022-01-15T16:29:49.806560",
+			"2022-04-05T17:01:49.484394",
+			"2022-07-02T22:42:06.780939"
+		]
+	}
+}
+```
+
+"song path" may be a full path, or without the user's music folder (e.g. /home/user/Music) in which case it is effectively a relative path from there. "plays" is mandatory (a list of ISO formatted timestamps for each time the song has been played in history), the rest are optional. For iceberg-not-ify purposes: If "artist" is not provided, will be assumed from the path (which is assumed to use an album artist/album folder structure), the others are not used.
+
+Intended for use with something like the "Song Change" plugin for Audacious or similar, and a script called from there. Presumably most media players at least have something among the lines of "call an external command when a song finishes", if not, that's unfortunate.
--- icebergify.py
+++ icebergify.py
@@ -1,24 +1,29 @@
-from flask import Flask, render_template, send_from_directory, redirect, session, request
+import base64
+from datetime import datetime, timedelta
+from functools import cache
+from io import BytesIO
+import os
+import re
+import sys
+from typing import Literal, OrderedDict
+from flask import Flask, render_template, send_from_directory, redirect, request
 from PIL import Image, ImageFont, ImageDraw
 import tekore as tk
 from werkzeug.middleware.proxy_fix import ProxyFix
-from flask_session import Session
 from fontTools.ttLib import TTFont
 from fontTools.unicode import Unicode
+import json
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = ''
 app.config['SESSION_TYPE'] = 'filesystem'
-Session(app)
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
-cid = ""
-secret = ""
-#redirect_uri = 'http://164.92.148.45:5000/callback'
-redirect_uri = 'https://icebergify.com/callback'
+cid = os.environ.get('SPOTIFY_CLIENT_ID')
+secret = os.environ.get('SPOTIFY_SECRET')
 
-cred = tk.Credentials(client_id=cid,client_secret=secret,redirect_uri=redirect_uri,sender=None, asynchronous=None)
+#cred = tk.Credentials(client_id=cid,client_secret=secret,redirect_uri=redirect_uri,sender=None, asynchronous=None)
+token = tk.request_client_token(cid, secret)
 sp = tk.Spotify()
 
 @app.route('/')
@@ -33,59 +38,94 @@ def privacy():
 def about():
     return render_template('about.html')
 
-@app.route('/berg.html')
+@cache
+def get_spotify_popularity(artist: str) -> int:
+    search_results = sp.search(artist, types=('artist',), limit=1)[0]
+    if not search_results.total:
+        #Not even found on Spotify! HIPSTER VICTORY!!!!
+        return -1
+    if search_results.items[0].name != artist:
+        #Unfortunately Spotify's API does not really let you do exact matches, you can put stuff in double quotes but it'll still return some other weird results sometimes
+        #So if this is not returning the artist we asked for, it is not on Spotify, therefore HIPSTER VICTORY!!!!!
+        return -1
+    return search_results.items[0].popularity
+
+def get_artist_playcount(term: timedelta=None) -> dict[str, int]:
+    #Spotify /me/top/artists API: long_term = "several years of data", medium_term = last 6 months, short_term = last 4 weeks
+    playcounts: dict[str, int] = {}
+    playlog_file = request.files['playlog']
+    playlog_file.seek(0)
+    play_log = json.load(playlog_file)
+    for track_path, track_info in play_log.items():
+        artist = track_info.get('artist')
+        if not artist:
+            artist = track_path.rsplit('/', 3)[-3]
+        if ', ' in artist or artist == 'Various Artists':
+            continue
+        for play in track_info['plays']:
+            play_time = datetime.fromisoformat(play)
+            if (not term) or ((datetime.now() - term) < play_time):
+                if artist not in playcounts:
+                    playcounts[artist] = 1
+                else:
+                    playcounts[artist] += 1
+
+    return playcounts
+
+@app.route('/berg.html', methods=['POST'])
 def berg():
-    ###AUTHENTICATION
-    user = session.get('state', None)
-    token = session.get('token', None)
-
-    if user is None or token is None:
-        session.pop('state', None)#
-        return redirect('/login')
-
-    if token.is_expiring:
-        token = cred.refresh(token)
-
     with sp.token_as(token):
         ###CREATE ICEBERG
 
-        name = sp.current_user().display_name
-        if  ' ' in name:
-            name = name[:name.index(' ')]
-        if '.' in name:
-            name = name[:name.index('.')]
+        name = request.form['name']
+        
+        #artists = {artist: get_spotify_popularity(artist) for artist, plays in sorted(get_artist_playcount(timedelta(days=2)).items(), key=lambda item: item[1])}
 
         artists = {}
-        for term in ["long_term", "medium_term", "short_term"]:
-            temp_artists = sp.current_user_top_artists(limit=50, offset=0, time_range=term)
-            for artist in temp_artists.items:
-                artists[artist.name] = artist.popularity
+        for term in [None, timedelta(days=30*6), timedelta(days=4 * 7)]:
+            temp_artists = OrderedDict(sorted(get_artist_playcount(term).items(), key=lambda item: item[1])[:50])
+            for artist in temp_artists.keys():
+                artists[artist] = get_spotify_popularity(artist)
 
-        sections = [(16,'first'),(28,'second'),(40,'third'),(52,'fourth'),(64,'fifth'),(76,'sixth'),(88,'seventh'),(100,'eighth')]
-        iceberg = {'first':[], 'second':[], 'third':[], 'fourth':[], 'fifth':[], 'sixth':[], 'seventh':[], 'eighth':[]}
+        # for term in ["long_term", "medium_term", "short_term"]:
+        #     temp_artists = sp.current_user_top_artists(limit=50, offset=0, time_range=term)
+
+        #     for artist in temp_artists.items:
+        #         artists[artist.name] = artist.popularity
+
+        sections: list[tuple[int, str]] = [(-1, 'hipster victory'), (16,'first'),(28,'second'),(40,'third'),(52,'fourth'),(64,'fifth'),(76,'sixth'),(88,'seventh'),(100,'eighth')]
+        iceberg: dict[str, list[str]] = {'first':[], 'second':[], 'third':[], 'fourth':[], 'fifth':[], 'sixth':[], 'seventh':[], 'eighth':[], 'hipster victory': []}
         min_pop = 0
         for max_pop in sections:
             for artist, pop in artists.items():
+                if pop == -1 and len(iceberg['hipster victory']) < 5:
+                    iceberg['hipster victory'].append(artist)
+                    continue
                 if (pop <= max_pop[0]) and (pop > min_pop) and len(iceberg[max_pop[1]]) < 5:
                     iceberg[max_pop[1]].append(artist)
             min_pop = max_pop[0]
 
         ###DRAW ICEBERG
-        for section, artists in iceberg.items():
-            artists_sorted = (sorted(artists, key=len))
+        for section, artists_in_section in iceberg.items():
+            artists_sorted = (sorted(artists_in_section, key=len))
             artists_sorted.reverse()
             iceberg[section] = artists_sorted
 
         ###FONT WORK
         intro = ImageFont.truetype("Intro Regular Regular.ttf", 55)
         noto = ImageFont.truetype("NotoSans-Regular.ttf", 55)
-        korean = ImageFont.truetype("NotoSansKR-Regular.otf", 55)
-        japanese = ImageFont.truetype("rounded-mgenplus-1cp-regular.ttf", 55)
+        #korean = ImageFont.truetype("NotoSansKR-Regular.otf", 55)
+        korean = ImageFont.truetype('NotoSansCJK-Regular.ttc', index=1)
+        #japanese = ImageFont.truetype("rounded-mgenplus-1cp-regular.ttf", 55)
+        japanese = ImageFont.truetype('NotoSansCJK-Regular.ttc', index=0)
 
         intro_ttf = TTFont('Intro Regular Regular.ttf')
-        noto_ttf = TTFont('NotoSans-Regular.ttf')
-        korean_ttf = TTFont('NotoSansKR-Regular.otf')
-        japanese_ttf = TTFont('rounded-mgenplus-1cp-regular.ttf')
+        #noto_ttf = TTFont('NotoSans-Regular.ttf')
+        noto_ttf = TTFont(noto.path)
+        #korean_ttf = TTFont('NotoSansKR-Regular.otf')
+        korean_ttf = TTFont(korean.path, fontNumber=1)
+        #japanese_ttf = TTFont('rounded-mgenplus-1cp-regular.ttf')
+        japanese_ttf = TTFont(japanese.path, fontNumber=0)
 
         def has_glyph(font, glyph):
             for table in font['cmap'].tables:
@@ -130,7 +170,16 @@ def berg():
 
         coordinates = {}
         coordinates['eighth'] = [[350, 335], [30,270], [640, 425], [150,415], [800,260]]
-        for i in range(6,-1,-1):
+        if iceberg['hipster victory']:
+            iceberg_img2_new = Image.new(iceberg_img2.mode, (iceberg_img2.width, iceberg_img2.height + 256))
+            iceberg_img2_new.paste(iceberg_img2)
+            iceberg_img2 = iceberg_img2_new
+            image2 = ImageDraw.Draw(iceberg_img2)
+        else:
+            iceberg.pop('hipster victory')
+            sections.pop(0)
+        #for i in range(6,-1,-1):
+        for i in range(7, -1, -1):
             if sections[i][1] != 'sixth':
                 coordinates[sections[i][1]] = []
                 for j in range(5):
@@ -148,37 +197,15 @@ def berg():
 
         ###TITLE
         intro_font_big = ImageFont.truetype("Intro Regular Regular.ttf", 100)
-        image2.text((25, 20), name + "'s Spotify", (19, 81, 143), intro_font_big)
+        image2.text((25, 20), name + "'s Not Spotify", (19, 81, 143), intro_font_big)
         image2.text((195, 120), "Iceberg", (19, 81, 143), intro_font_big)
 
         ###SAVING
-        sid = sp.current_user().id
-        bergname = "result" + str(sid) + ".jpg"
-        iceberg_img2.save("data/" + str(bergname))
+        bytesio = BytesIO()
+        iceberg_img2.save(bytesio, format='png')
+        encoded = base64.b64encode(bytesio.getvalue()).decode('ascii')
 
-    return render_template('berg.html', value=bergname)
-
-@app.route('/login', methods=['GET'])
-def login():
-    if session.get('token', None) is not None:
-        return redirect('/berg.html')
-
-    scope = tk.scope.user_top_read
-    auth = tk.UserAuth(cred, scope)
-    #session['auth'] = auth.__dict__
-    return redirect(auth.url)
-
-@app.route('/callback', methods=['GET'])
-def callback():
-    code = request.args.get('code')
-    state = request.args.get('state')
-
-    #check if state matches auth state somehow
-    token = cred.request_user_token(code)
-    #token = auth.request_token(code, state)
-    session['state'] = state
-    session['token'] = token
-    return redirect('/berg.html')
+    return render_template('berg.html', value=encoded, height=iceberg_img2.height // 2)
 
 @app.route('/data/<filename>')
 def display_image(filename):
